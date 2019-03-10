a9d56f8f933beb8eddc8a4f82ce34cc5daf3db21
Doug Johnson <dougvj@gmail.com>
2019-03-10 05:55:43 -0600

    Rework parameter parsing FOLD IN

--- main.c
+++ main.c
@@ -2,19 +2,169 @@
 #include <stdio.h>
 #include "trace.h"
 #include <time.h>
+#include <getopt.h>
+#include <unistd.h>
+#include <string.h>
 #include "geometric_primitives.h"
 //#include "window.h"
 #include "bmp.h"
+
+typedef struct {
+    int w;
+    int h;
+    int num_threads;
+    int num_frames;
+    int max_bounces;
+    int path_tracing_enabled;
+    int window_enabled;
+    int win_w;
+    int win_h;
+} program_parameters;
+
 
 float frand() {
     return rand() / (float)RAND_MAX;
 }
 
-int main(int argc, char** argv) {
-    if (argc < 7) {
-        fprintf(stderr, "Usage:\n\traytracer <width> <height> <num_threads> <max_bounces> <rays_per_pixel> <num_frames>\n");
-        return 1;
+static const char usage_string[] = 
+"\
+Options:\n\
+    --res <width>x<height> Required output resolution argument, ie 800x600\n\
+    --num-threads <num>   Number of worker threads\n\
+                          Default is machine size\n\
+    --max-bounces <num>   The maximum number of ray traced bounces.\n\
+                          The default is 8\n\
+    --enable_path_tracing Set to enable full monte-carlo path tracing\n\
+                          For soft shadows, global illumination, etc\n\
+    --enable-window       Enable output window for live viewing of output\n\
+    --window-res <width>x<height> Resolution of output window if enabled\n\
+";
+
+static void print_usage_and_quit() {
+    fprintf(stderr, "%s", usage_string);
+    exit(1);
+}
+
+static program_parameters params;
+
+static int parse_int(char* arg) {
+    //TODO check valid integer with different function
+    return atoi(arg);
+}
+
+static void parse_resolution_arg(char* arg, int* x, int* y) {
+    char* res = arg;
+    char* w = res;
+    char* h;
+    //find the 'x' part
+    for (h = res; *h != '\0' && *h != 'x'; h++);
+    if (*h == '\0') {
+        fprintf(stderr, "Expected <width>x<height>\n"); 
+        exit(1);
     }
+    h++;
+    *x = parse_int(w);
+    *y = parse_int(h);
+    fprintf(stderr, "Got %i, %i\n", *x, *y);
+}
+
+static void parse_args(int argc, char** argv) {
+    for(;;) {
+        //Declare our options scoped in for
+        static struct option options[] = {
+            //required arguments
+            {
+                .name = "res",
+                .has_arg = required_argument 
+            },
+            //optional arguments with defaults
+            {
+                .name = "num-threads",
+                .has_arg = required_argument
+            },
+            {
+                .name = "max-bounces",
+                .has_arg = required_argument
+            },
+            {
+                .name = "enable-path-tracing",
+                .flag = &(params.path_tracing_enabled),
+                .val = 1
+            },
+            {
+                .name = "samples-per-pixel",
+                .has_arg = required_argument
+            },
+            {
+                .name = "num-frames",
+                .has_arg = required_argument
+            },
+            {
+                .name = "enable-window",
+                .flag = &(params.window_enabled),
+                .val = 1
+            },
+            {
+                .name = "help",
+            }
+        };
+        int i;
+        int c = getopt_long(argc, argv, "", options, &i);
+        if (c == -1)
+            break;
+        switch(c) {
+            case 0:
+                printf("getopt returned %c 0x%X\n", c, (int) c);
+                printf("option %s", options[i].name);
+                if (options[i].flag != 0) {
+                    printf(" is %s\n", (*(options[i].flag) ? "set": "not set"));
+                } else {
+                    printf(" with arg %s\n", optarg);
+                }
+                switch(i) {
+                    case 0: { //"res" parameter
+                        parse_resolution_arg(optarg, &(params.w), &(params.h));
+                    } break;
+                    case 1: { //num threads parameter
+                        params.num_threads = parse_int(optarg);
+                    } break;
+                    case 7: {
+                        print_usage_and_quit();
+                    } break;
+                }
+                break;
+            default:
+                print_usage_and_quit();
+        }
+    }
+}
+
+static void print_parameters() {
+    fprintf(stderr, "Running with the following parameters:\n");
+    fprintf(stderr, "\tOutput Resolution: %ix%i\n", params.w, params.h);
+    fprintf(stderr, "\tWindowed Output: %s\n", params.window_enabled ? "True" : "False");
+    fprintf(stderr, "\tNum Worker Threads: %i\n", params.num_threads);
+    fprintf(stderr, "\tMax Bounces: %i\n", params.max_bounces);
+}
+
+int main(int argc, char** argv) {
+    //Set default parameter values
+    //Most values are 0 by default
+    memset(&params, 0, sizeof(program_parameters));
+    //Set number of threads to system's hardware threads
+    params.num_threads = sysconf(_SC_NPROCESSORS_ONLN);
+    //Sensible Defaults
+    params.max_bounces = 8;
+    params.num_frames = 1;
+    //Parse the arguments
+    parse_args(argc, argv);
+    //Validate parameters
+    if (params.w == 0 || params.h == 0) {
+        fprintf(stderr, "Output resolution not set with --res\n");
+        exit(1);
+    }
+    print_parameters();
+    print_usage_and_quit();
     //srand(100);
     srand(time(NULL));
     int num_frames = atoi(argv[6]);
@@ -106,7 +256,7 @@ int main(int argc, char** argv) {
     }
     //Allocate the output frame buffer
     char* output_buffer = aligned_malloc(64, sizeof(char[3]) * w * h);
-    render_parameters params = (render_parameters){
+    render_parameters render_params = (render_parameters){
         .x = w,
         .y = h,
         .num_threads = num_threads,
@@ -119,9 +269,9 @@ int main(int argc, char** argv) {
 
     };
     for (int i = 0; i < num_frames; i++) {
-        params.origin_z = -10 + (i/10.0);
+        render_params.origin_z = -10 + (i/10.0);
     	fprintf(stderr, "Generating frame %d\n", i);
-    	renderScene(rc, &params);
+    	renderScene(rc, &render_params);
         char filename[256];
         sprintf(filename, "render_output/%d.bmp", i);
         printf("Dumping frame to %s...\n", filename);
